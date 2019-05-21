8bc990fc0d406996b60ff8c121e8ac0e6084a7e9
Komal <Komallearninggit.com>
2019-05-21 15:01:14 +0530

    The application is running 100%

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -64,6 +64,10 @@ class AdminCategoriesController extends Controller
     public function edit($id)
     {
         //
+
+        $category=Category::findOrFail($id);
+        return View('admin.categories.edit', compact('category'));
+
     }
 
     /**
@@ -76,6 +80,10 @@ class AdminCategoriesController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $category= Category::findOrFail($id);
+        $category->update($request->all());
+        return redirect('/admin/categories');
+
     }
 
     /**
@@ -87,5 +95,8 @@ class AdminCategoriesController extends Controller
     public function destroy($id)
     {
         //
+Category::findOrFail($id)->delete();
+return redirect('/admin/categories');
+
     }
 }
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -6,34 +6,31 @@
 
 <div class="col-sm-6">
 
-    {!! Form::open(['method'=>'PATCH', 'action'=>'AdminCategoriesController@update', $category->id])!!}
+    {!! Form::model($category, ['method'=>'PATCH', 'action'=>['AdminCategoriesController@update', $category->id]])!!}
     <div class="form-group">
 {!!Form::label('name', 'Name: ')!!}
 {!!Form::text('name', null,['class'=>'form-control'])!!}
     </div>
 
 <div class="form-group">
-{!! Form::submit('Create Category', ['class'=>'btn btn-primary'])!!}
+{!! Form::submit('Update Category', ['class'=>'btn btn-primary col-sm-6'])!!}
 </div>
     {!!Form::close()!!}
+    {!! Form::model($category, ['method'=>'DELETE', 'action'=>['AdminCategoriesController@destroy', $category->id]])!!}
+
+  <div class="form-group">
+  {!! Form::submit('Delete Category', ['class'=>'btn btn-danger col-sm-6'])!!}
+  </div>
+    {!!Form::close()!!}
+
+
 </div>
 
-@extends('layouts.admin')
 
-@section('content')
-<h1> Categories </h1>
+
 
 
 <div class="col-sm-6">
-
-    {!! Form::open(['method'=>'DELETE', 'action'=>'AdminCategoriesController@destroy', $category->id])!!}
-    <div class="form-group">
-{!!Form::label('name', 'Name: ')!!}
-{!!Form::text('name', null,['class'=>'form-control'])!!}
-    </div>
-
-<div class="form-group">
-{!! Form::submit('Create Category', ['class'=>'btn btn-primary'])!!}
 </div>
-    {!!Form::close()!!}
-</div>
+
+@stop
